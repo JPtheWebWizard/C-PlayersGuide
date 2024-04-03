a5dad73d15d34dae934da535394856771ea0137b
James C <151131912+JPtheWebWizard@users.noreply.github.com>
2024-04-03 00:27:18 -0400

    Add files via upload

---
+++ CH4_TheMakingsOfAProgrammer.cs
@@ -0,0 +1,8 @@
+﻿// Challenge 4: The Makings of a Programmer
+// Page 24 - 50XP
+
+Console.WriteLine("Hello once again.");
+Console.WriteLine("There's technically no limit to the number of statements that can be executed within one program.");
+Console.WriteLine("I assume it primarily depends on factors such as available");
+Console.WriteLine("memory and the complexity of the program.");
+Console.WriteLine("Goodbye.");
