cf05b9f3bd27761461e29a54fc0d0e76fea3f06f
James C <151131912+JPtheWebWizard@users.noreply.github.com>
2024-04-03 02:16:14 -0400

    Add files via upload

---
+++ CH5_ConsolasAndTelim.cs
@@ -0,0 +1,8 @@
+﻿// Challenge 5: Consolas and Telim
+// Page 26 - 50XP
+
+string name;
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+name = Console.ReadLine();
+Console.WriteLine("Noted: " + name + " got bread.");
