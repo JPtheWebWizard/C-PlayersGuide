12cff1a7e23d7a50f1aaaa4ecd62e2dace6afb9f
James C <151131912+JPtheWebWizard@users.noreply.github.com>
2024-04-05 00:41:11 -0400

    Add files via upload

---
+++ L6_CH8_TheVariableShopReturns.cs
@@ -0,0 +1,51 @@
+﻿/*
+* Level 6 ﻿Challenge 8: The Variable Shop Returns
+* Page 46 - 50XP
+*/
+
+
+int a = 2;
+uint b = 2;
+short c = 2;
+ushort d = 2;
+long e = 2;
+ulong f = 2;
+byte g = 2;
+sbyte h = 2;
+char i = '3';
+string j = "3";
+bool k = true;
+float l = 3.0f;
+double m = 3.0;
+decimal n = 3.0m;
+
+
+a = 3;
+b = 3;
+c = 3;
+d = 3;
+e = 3;
+f = 3;
+g = 3;
+h = 3;
+i = '2';
+j = "2";
+k = false;
+l = 2.0f;
+m = 2.0;
+n = 2.0m;
+
+Console.WriteLine("Type 1: " + a);
+Console.WriteLine("Type 2: " + b);
+Console.WriteLine("Type 3: " + c);
+Console.WriteLine("Type 4: " + d);
+Console.WriteLine("Type 5: " + e);
+Console.WriteLine("Type 6: " + f);
+Console.WriteLine("Type 7: " + g);
+Console.WriteLine("Type 8: " + h);
+Console.WriteLine("Type 9: " + i);
+Console.WriteLine("Type 10: " + j);
+Console.WriteLine("Type 11: " + k);
+Console.WriteLine("Type 12: " + l);
+Console.WriteLine("Type 13: " + m);
+Console.WriteLine("Type 14: " + n);
